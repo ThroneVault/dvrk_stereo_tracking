79eabc43322acc9c0925364427e01663ce7c23da
Feroze Naina <ferozenaina@gmail.com>
2017-01-04 19:56:48 -0500

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,38 @@
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include "video_wrapper.hpp"
-#include "frame_processor.hpp"
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
+#include "stereo_tracking.hpp"
+
+using namespace std;
 
 int main(int argc, char *argv[]) {
 
 
-  /*
-   * TODO
-   *
-   * convert to catkin project
-   * publish topic directly
-   * write python code to publish to transform
-   * camera calibration can be used as a yaml file
-   */
+  ros::init(argc, argv, "image_listener");
 
+  StereoTracking st;
 
-
-      ros::init(argc, argv, "image_listener");
-
-  VideoWrapper left_vw = VideoWrapper();
-  //FrameProcessor left_fp = FrameProcessor();
-
-  //VideoWrapper right_vw = VideoWrapper(argv[2]);
-  //FrameProcessor right_fp = FrameProcessor();
-
-  //Mat left_frame, right_frame, left_thresholded_frame, right_thresholded_frame;
-  //Point left_center, right_center;
-
-  //Rect crop_rect(250,0 ,390, 480);
-  //left_vw.SetCrop(crop_rect);
-  //right_vw.SetCrop(crop_rect);
-
-
-  //// TODO check if frame exists
   //while (1)
   //{
-    //left_frame = left_vw.GetFrame();
-    //left_center = left_fp.GetRedCenter(left_frame);
-    //left_thresholded_frame = left_fp.GetThresholdedImage(left_frame);
-    //cout << "left_center is [" << left_center.x << "," << left_center.y << "]"<< endl;
-    //imshow("left", left_thresholded_frame);
-    //waitKey(0);
 
-    //right_frame = right_vw.GetFrame();
-    //right_center = right_fp.GetRedCenter(right_frame);
-    //right_thresholded_frame = right_fp.GetThresholdedImage(right_frame);
-    //cout << "right_center is [" << right_center.x << "," << right_center.y << "]"<< endl;
-    imshow("right", left_vw.GetFrame());
-    waitKey(0);
-
-    //// open camera matrix and triangulate point
-
-  //}
-  //
-  
-  cout << "1) Set Crop\n2) View Crop\n3)Set Color\n4)View Color\n5)Start Tracking\nPress Ctrl-C to exit";
+  cout << "1) View Crop\n2)View Thresholded\n3)Start Tracking\n Ctrl-C to exit";
   int option;
-  cin >> option;
+  //cin >> option;
 
-  switch (option)
-  {
-    case 1: cout << "1 selected";
-            break;
-    case 2: cout << "2 selected";
-            break;
-    default: cout << "invalid option";
-            break;
-  }
+  //switch (option)
+  //{
+  //case 1: cout << "1 selected";
+  //viewCrop();
+  //break;
+  //case 2: cout << "2 selected";
+  st.ViewThresholded();
+  //break;
+  //case 2: cout << "2 selected";
+  //st.TrackBlob();
+  //break;
+  //case 0: exit(0);
+  //break;
+  //default: cout << "invalid option";
+  //break;
+  //}
+  //}
 
 
   return 0;
